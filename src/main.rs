e628b01d3127239fc36b5b7d8d77bc57544b6b3e
smalnote <smalnote@gmail.com>
2024-07-24 21:34:20 +0800

    dependcies: time & rand

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use rand::Rng;
+use time::OffsetDateTime;
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = rand::thread_rng();
+    let random_number: u32 = rng.gen();
+    let now = OffsetDateTime::now_utc();
+    println!("Hello, world! @{now} with {}",random_number);
 }
