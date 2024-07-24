e91373b120e60e892ad2ddbc8dc3353a28c9e900
smalnote <smalnote@gmail.com>
2024-07-25 03:27:56 +0800

    lsc: multiple bin & bin guess

---
+++ src/bin/guess/main.rs
@@ -0,0 +1,35 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io::{self, Write};
+
+fn main() {
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    print!("Guess the number!\n");
+    loop {
+        print!("Please input your guess:");
+        io::stdout().flush().expect("Failed to flush stdout");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = match guess
+            .trim()
+            .parse() {
+                Ok(num) => num,
+                Err(_) => {
+                    println!("Please input a number!");
+                    continue;
+                }
+            };
+        println!("You guessed: {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small! Try again."),
+            Ordering::Greater => println!("Too big! Try again."),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+    println!("The secret number is: {secret_number}");
+}
--- src/main.rs
+++
@@ -1,9 +0,0 @@
-use rand::Rng;
-use time::OffsetDateTime;
-
-fn main() {
-    let mut rng = rand::thread_rng();
-    let random_number: u32 = rng.gen();
-    let now = OffsetDateTime::now_utc();
-    println!("Hello, world! @{now} with {}",random_number);
-}
