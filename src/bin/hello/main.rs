a9b04d4d71ccb4391d2e152b1eab8f920e9b30a1
smalnote <smalnote@gmail.com>
2024-07-26 19:34:38 +0800

    hello: string ref

--- src/bin/hello/main.rs
+++ src/bin/hello/main.rs
@@ -2,11 +2,12 @@ use rand::Rng;
 use time::OffsetDateTime;
 
 fn main() {
+    let greeting: &str = "Hello, world!";
     let mut rng = rand::thread_rng();
     let random_number: i32 = rng.gen_range(0..100);
     let now = OffsetDateTime::now_local().expect("Failed to get local date time");
     // marcos, generated code by function arguments.
-    println!("Hello, world! @{now} with {}", add(random_number, random_number));
+    println!("{greeting} @{now} with {}", add(random_number, random_number));
 }
 
 fn add(a: i32, b: i32) -> i32 {
