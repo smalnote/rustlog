5ca093d083375d4ccd9dfc1441f6912a2930a876
smalnote <smalnote@gmail.com>
2024-07-26 13:02:51 +0800

    hello: enable local-offset feature

--- src/bin/hello/main.rs
+++ src/bin/hello/main.rs
@@ -4,7 +4,7 @@ use time::OffsetDateTime;
 fn main() {
     let mut rng = rand::thread_rng();
     let random_number: i32 = rng.gen_range(0..100);
-    let now = OffsetDateTime::now_utc();
+    let now = OffsetDateTime::now_local().expect("Failed to get local date time");
     // marcos, generated code by function arguments.
     println!("Hello, world! @{now} with {}", add(random_number, random_number));
 }
