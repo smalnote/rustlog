4bbf192421219dadc1a48e797fe2ca232ff7317c
smalnote <smalnote@gmail.com>
2024-08-08 21:02:59 +0800

    generic: impl trait, &dyn and Box<dyn >

--- src/bin/hello/main.rs
+++ src/bin/hello/main.rs
@@ -1,6 +1,7 @@
 use std::ops::{Range, RangeInclusive};
 
 use rand::Rng;
+use std::fmt::Debug;
 use time::OffsetDateTime;
 
 fn main() {
@@ -1504,5 +1505,363 @@ fn main() {
             Array { data: [1, 2, 3] },
             Array { data: [1, 2, 3] },
         ];
+    }
+
+    // trait, trait as parameter type, trait bound
+    {
+        trait Animal {
+            fn sound(&self) -> String;
+        }
+
+        struct Sheep;
+        struct Cow;
+
+        impl Animal for Sheep {
+            fn sound(&self) -> String {
+                "Maah".to_string()
+            }
+        }
+
+        impl Animal for Cow {
+            fn sound(&self) -> String {
+                "Mooh".to_string()
+            }
+        }
+
+        fn bark(animal: &impl Animal) {
+            println!("barking: {}", animal.sound());
+        }
+
+        bark(&Sheep);
+
+        fn bark_both<T: Animal>(a: &T, b: &T) {
+            println!("barking both: {}, {}", a.sound(), b.sound());
+        }
+
+        bark_both(&Cow, &Cow);
+    }
+
+    /*
+     * Derivable Traits
+     * Trait taht can be automatically implemented for a struct or an enum by the Rust compiler
+     * Called "derivable" because they can be derived automatically
+     * Most common derivable traits:
+     *   - Debug: Allowing to output content via "{:?}"
+     *   - Clone: Enables type to be duplicated with "clone()" method
+     *   - Copy: Enables type to be copied implicity, without requiring explicit "clone()" method
+     *   - PartialEq: Enables comparison
+     */
+    {
+        #[derive(Debug, Clone, Copy, PartialEq)]
+        struct Point<T> {
+            x: T,
+            y: T,
+        }
+
+        let p = Point { x: 3.1, y: 4.1 };
+        let q = p.clone();
+        assert_eq!(p, q);
+
+        fn use_clone_eq<T: Clone + PartialEq + std::fmt::Debug>(a: &T, b: &T) {
+            assert_eq!(a.clone(), b.clone())
+        }
+        use_clone_eq(&p, &q);
+    }
+
+    // where clauses for heavy use of trait bounds
+    {
+        #[derive(Debug, Clone, Copy, PartialEq)]
+        struct Point<T> {
+            x: T,
+            y: T,
+        }
+
+        fn use_clone_eq<T, U>(a: &T, b: &U)
+        where
+            T: Clone + PartialEq + Debug,
+            U: Clone + Debug,
+        {
+            assert_eq!(*a, a.clone());
+            println!("b = {:?}", b.clone());
+        }
+
+        let p = Point { x: 'a', y: 'b' };
+        let q = Point {
+            x: "hello",
+            y: "world",
+        };
+        use_clone_eq(&p, &q);
+    }
+
+    // trait as return type
+    {
+        trait Animal {}
+
+        struct Cat;
+        struct Dog;
+
+        impl Animal for Cat {}
+        impl Animal for Dog {}
+
+        fn return_dog() -> impl Animal {
+            Dog
+        }
+
+        fn return_cat() -> impl Animal {
+            Cat
+        }
+
+        let _dog = return_dog();
+        let _cat = return_cat();
+    }
+
+    // trait with default method
+    {
+        trait Hello {
+            fn say_hello(&self) -> String {
+                "hello".to_string()
+            }
+
+            fn say_something(&self) -> String;
+        }
+
+        struct Student;
+        struct Teacher;
+
+        impl Hello for Student {
+            fn say_something(&self) -> String {
+                "I'm a bad student!".to_string()
+            }
+        }
+
+        impl Hello for Teacher {
+            fn say_hello(&self) -> String {
+                "greeting".to_string()
+            }
+
+            fn say_something(&self) -> String {
+                "I'm not a teacher!".to_string()
+            }
+        }
+
+        let s = Student;
+        assert_eq!(s.say_hello(), "hello");
+        assert_eq!(s.say_something(), "I'm a bad student!");
+
+        let t = Teacher;
+        assert_eq!(t.say_hello(), "greeting");
+        assert_eq!(t.say_something(), "I'm not a teacher!");
+    }
+
+    // destruct struct tuple
+    {
+        struct Centimeters(f64);
+        struct Inches(i32);
+        impl Inches {
+            fn to_centimeters(&self) -> Centimeters {
+                let &Inches(inches) = self; // destruct struct tuple
+                Centimeters(inches as f64 * 2.54)
+            }
+        }
+
+        let five_inches = Inches(5);
+        let cm = five_inches.to_centimeters();
+        let Centimeters(cm) = cm; // destruct struct tuple and shadow variable cm
+        assert_eq!(cm, 2.54 * 5.0);
+    }
+
+    // derive PartialEq, PartialOrd
+    {
+        #[derive(Debug, PartialEq, PartialOrd)]
+        struct Seconds(u32);
+
+        assert_eq!(Seconds(10), Seconds(10));
+        assert!(Seconds(10) == Seconds(10));
+        assert!(Seconds(20) > Seconds(10));
+    }
+
+    // operator trait
+    {
+        #[derive(Debug, PartialEq)]
+        struct Centimeters(f64);
+        #[derive(Debug, PartialEq)]
+        struct SquareCentimeters(f64);
+
+        impl std::ops::Mul for Centimeters {
+            type Output = SquareCentimeters;
+            fn mul(self, rhs: Self) -> SquareCentimeters {
+                let Centimeters(lhs) = self;
+                let Centimeters(rhs) = rhs;
+                SquareCentimeters(lhs * rhs)
+            }
+        }
+
+        assert_eq!(Centimeters(5.0) * Centimeters(6.0), SquareCentimeters(30.0));
+    }
+
+    // operator trait with different type of right hand side
+    {
+        struct Foo;
+        struct Bar;
+
+        #[derive(Debug, PartialEq)]
+        struct FooBar;
+        #[derive(Debug, PartialEq)]
+        struct BarFoo;
+
+        impl std::ops::Add<Bar> for Foo {
+            type Output = FooBar;
+            fn add(self, _rhs: Bar) -> Self::Output {
+                FooBar
+            }
+        }
+
+        impl std::ops::Sub<Bar> for Foo {
+            type Output = BarFoo;
+            fn sub(self, _rhs: Bar) -> Self::Output {
+                BarFoo
+            }
+        }
+
+        assert_eq!(Foo + Bar, FooBar);
+        assert_eq!(Foo - Bar, BarFoo);
+    }
+
+    /*
+     * Dynamic Dispatch (&dyn Trait, Box<dyn Trait> )
+     *   - Specific methods to be called is determined at runtime
+     *   - Works by creating a reference(`&dyn`) or smart pointer(`Box<dyn >`) to a trait object
+     *   - Compiler builds a `vtable` for a trait when a trait instance object is created
+     *   - `vtable` contains a pointer to the implementation of each method in the trait for the specific type of the object
+     *   - Compiler will do a lookup in a vtable to determine which method should be called for which type that implements the given trait
+     *   - This lookup will cause overhead but allows for more flexible code
+     */
+    // dynamic trait object for return unknown-size trait instance
+    // &dyn has ownership
+    // &dyn T vs &T (Box<dyn T> vs Box<T>), &dyn T, Box<dyn T> is also called fat pointer
+    // &dyn T has a `ptr` point to instance and a `vptr` point to the corresponding `vtable`
+    // &T has a `ptr` point to instance in heap
+    {
+        trait Animal {
+            fn sound(&self) -> String;
+        }
+
+        struct Sheep;
+        struct Cow;
+
+        impl Animal for Sheep {
+            fn sound(&self) -> String {
+                "Maah".to_string()
+            }
+        }
+
+        impl Animal for Cow {
+            fn sound(&self) -> String {
+                "Mooh".to_string()
+            }
+        }
+
+        fn new_animal(s: &str) -> &dyn Animal {
+            match s {
+                "sheep" => &Sheep,
+                "cow" => &Cow,
+                _ => panic!("unknown animal"),
+            }
+        }
+        fn bark(animal: &(impl Animal + ?Sized)) {
+            println!("barking: {}", animal.sound());
+        }
+
+        let animals: [&dyn Animal; 3] =
+            [new_animal("sheep"), new_animal("cow"), new_animal("sheep")];
+        for animal in animals {
+            bark(animal);
+        }
+
+        // return &dyn Animal value moved to _cow
+        let mut _cow = new_animal("cow");
+        // validate that _cow move to _cow_moved
+        let _cow_moved = _cow;
+    }
+
+    // dynamic dispatch by using Box<dyn >
+    {
+        trait Animal {
+            fn sound(&self) -> String;
+        }
+
+        struct Sheep;
+        struct Cow;
+
+        impl Animal for Sheep {
+            fn sound(&self) -> String {
+                "Maah".to_string()
+            }
+        }
+
+        impl Animal for Cow {
+            fn sound(&self) -> String {
+                "Mooh".to_string()
+            }
+        }
+
+        fn new_animal(a: &str) -> Box<dyn Animal> {
+            match a {
+                "sheep" => Box::new(Sheep),
+                "cow" => Box::new(Cow),
+                _ => panic!("unknown animal"),
+            }
+        }
+
+        let animals = [new_animal("sheep"), new_animal("sheep"), new_animal("cow")];
+        for animal in animals {
+            println!("unbox dyn Animal sound: {}", animal.sound())
+        }
+    }
+
+    /*
+     * | ----------|-----------------------------------|------------------------------------------|
+     * |           | & (reference)                     | Box                                      |
+     * | ----------|-----------------------------------|------------------------------------------|
+     * | Memory    | Only points to a value alread in  | Allocates data on heap and owns it, also |
+     * |           | memory.                           | responsible for deallocating when values |
+     * |           |                                   | goes out of scope.                       |
+     * | ----------|-----------------------------------|------------------------------------------|
+     * | Lifetime  | Limited                           | Can be passed across scopes              |
+     * | ----------|-----------------------------------|------------------------------------------|
+     * | Clonable  | No                                | Yes                                      |
+     * | ----------|-----------------------------------|------------------------------------------|
+     * | Pattern   | No                                | Yes                                      |
+     * | ----------|-----------------------------------|------------------------------------------|
+     */
+
+    // generic, impl for specific trait
+    {
+        struct Pair<T> {
+            x: T,
+            y: T,
+        }
+
+        impl<T> Pair<T> {
+            fn new(x: T, y: T) -> Self {
+                Self { x, y }
+            }
+        }
+
+        impl<T: std::fmt::Debug + PartialEq + PartialOrd> Pair<T> {
+            fn cmp_display(&self) {
+                if self.x >= self.y {
+                    println!("The larger member is x = {:?}", self.x);
+                } else {
+                    println!("The larger member is y = {:?}", self.y);
+                }
+            }
+        }
+
+        #[derive(Debug, PartialEq, PartialOrd)]
+        struct Unit(f32);
+
+        let pair = Pair::new(Unit(3.4), Unit(4.3));
+        pair.cmp_display();
     }
 }
