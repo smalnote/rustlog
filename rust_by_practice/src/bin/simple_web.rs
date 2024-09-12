eebff29171fabcd298ac861b41ab2dfe107ac9b2
smalnote <smalnote@gmail.com>
2024-09-12 22:09:10 +0800

    web: http server  by tcp
    Thread pool for large throughput
    Graceful shutdown with Ctrl-C

---
+++ rust_by_practice/src/bin/simple_web.rs
@@ -0,0 +1,85 @@
+use rust_by_practice::ThreadPool;
+use std::{
+    io::{BufRead, BufReader, Read, Write},
+    net::{TcpListener, TcpStream},
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
+};
+
+fn main() {
+    const ADDR: &str = "127.0.0.1:7878";
+    let listener = TcpListener::bind(ADDR).unwrap();
+
+    println!("Start listening on: {}...", listener.local_addr().unwrap());
+    let running = Arc::new(AtomicBool::new(true));
+
+    let running_ctrlc = Arc::clone(&running);
+    ctrlc::set_handler(move || {
+        running_ctrlc.store(false, Ordering::SeqCst);
+        println!("Shutting down server...");
+        // trigger loop on listener.incoming() once
+        send_programmatic_tcp_connection(ADDR);
+    })
+    .unwrap();
+
+    let pool = ThreadPool::new(4);
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+        if !running.load(Ordering::SeqCst) {
+            break;
+        }
+    }
+}
+
+const HELLO_PAGE: &str = "<!DOCTYPE html>
+<html lang=\"en\">
+  <head>
+    <meta charset=\"utf-8\">
+    <title>Hello!</title>
+  </head>
+  <body>
+    <h1>Hello!</h1>
+    <p>Hi from Rust</p>
+  </body>
+</html>
+";
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let response = if request_line == "GET / HTTP/1.1" {
+        format!(
+            "{}\r\rContent-Length: {}\r\n\r\n{}",
+            "HTTP/1.1 200 OK",
+            HELLO_PAGE.len(),
+            HELLO_PAGE
+        )
+    } else {
+        "HTTP/1.1 404 NOT FOUND\n\n".to_string()
+    };
+    stream.write_all(response.as_bytes()).unwrap();
+}
+
+// Programmatically sending a connection to the listener
+fn send_programmatic_tcp_connection(addr: &str) {
+    // Connect to the server
+    if let Ok(mut stream) = TcpStream::connect(addr) {
+        println!("Sending message to server...");
+        stream.write(b"OPTIONS / HTTP/1.1\r\n").unwrap();
+        stream.flush().unwrap();
+
+        let mut buffer = [0; 512];
+        stream.read(&mut buffer).unwrap();
+        println!(
+            "Response from server: {}",
+            String::from_utf8_lossy(&buffer[..])
+        );
+    } else {
+        eprintln!("Failed to connect to server");
+    }
+}
--- rust_by_practice/src/lib.rs
+++ rust_by_practice/src/lib.rs
@@ -9,6 +9,14 @@ pub mod restaurant;
 
 pub mod practice;
 
+use std::{
+    sync::{
+        mpsc::{self, Receiver, Sender},
+        Arc, Mutex,
+    },
+    thread,
+};
+
 // re-export function with a short namespace rustlog::add_one
 pub use self::practice::p320_documentation::add_one;
 
@@ -24,3 +32,79 @@ mod tests {
         assert_eq!(z, 43);
     }
 }
+
+pub struct ThreadPool {
+    sender: Option<Sender<Job>>,
+    threads: Vec<Worker>,
+}
+
+impl ThreadPool {
+    /// Create a new ThreadPool
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+        let mut threads = Vec::with_capacity(size);
+        let (sender, receiver) = mpsc::channel::<Job>();
+        let receiver = Arc::new(Mutex::new(receiver));
+        for id in 0..size {
+            threads.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+        ThreadPool {
+            sender: Some(sender),
+            threads,
+        }
+    }
+
+    pub fn execute<F>(&self, func: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        self.sender.as_ref().unwrap().send(Box::new(func)).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        drop(self.sender.take());
+        for worker in &mut self.threads {
+            println!("Shutting down worker {}", worker.id);
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            match receiver.lock().unwrap().recv() {
+                Ok(job) => {
+                    println!("Worker {} got a job; executing.", id);
+                    job();
+                }
+                Err(_) => {
+                    println!("Worker {} stopped.", id);
+                    break;
+                }
+            }
+        });
+
+        Worker {
+            id,
+            thread: Some(thread),
+        }
+    }
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
