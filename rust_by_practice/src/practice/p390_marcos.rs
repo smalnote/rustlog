0d391e438730bdaa4f05656fde5bd3590f516309
smalnote <smalnote@gmail.com>
2024-09-12 18:48:17 +0800

    marco: length add trait for different units

--- rust_by_practice/src/practice/p390_marcos.rs
+++ rust_by_practice/src/practice/p390_marcos.rs
@@ -35,4 +35,70 @@ mod tests {
 
         Pancakes::hello_marco();
     }
+
+    #[test]
+    fn test_marco_for_length_unit_calculation() {
+        use length::AddLengths;
+        use std::ops::Add;
+
+        #[derive(Debug, PartialEq, AddLengths)]
+        struct Millimeters(f64);
+
+        #[derive(Debug, PartialEq, AddLengths)]
+        struct Meters(f64);
+
+        #[derive(Debug, PartialEq, AddLengths)]
+        struct Kilometers(f64);
+
+        impl From<Meters> for Millimeters {
+            fn from(value: Meters) -> Self {
+                Millimeters(value.0 * 1000.0)
+            }
+        }
+
+        impl From<Kilometers> for Millimeters {
+            fn from(value: Kilometers) -> Self {
+                Millimeters(value.0 * 1_000_000.0)
+            }
+        }
+
+        impl From<Millimeters> for Meters {
+            fn from(value: Millimeters) -> Self {
+                Meters(value.0 / 1000.0)
+            }
+        }
+
+        impl From<Kilometers> for Meters {
+            fn from(value: Kilometers) -> Self {
+                Meters(value.0 * 1000.0)
+            }
+        }
+
+        impl From<Millimeters> for Kilometers {
+            fn from(value: Millimeters) -> Self {
+                Kilometers(value.0 / 1_000_000.0)
+            }
+        }
+
+        impl From<Meters> for Kilometers {
+            fn from(value: Meters) -> Self {
+                Kilometers(value.0 / 1000.0)
+            }
+        }
+
+        assert_eq!(Millimeters(1.0) + Meters(1.0), Millimeters(1001.0));
+        assert_eq!(
+            Millimeters(1.0) + Meters(1.0) + Kilometers(1.0),
+            Millimeters(1001001.0)
+        );
+        assert_eq!(
+            Meters(1.0) + Millimeters(500.0) + Kilometers(2.0),
+            Meters(2001.5)
+        );
+
+        assert_eq!(
+            Kilometers(1.0) + Meters(1.0) + Millimeters(2_000.0),
+            Kilometers(1.003)
+        );
+    }
 }
