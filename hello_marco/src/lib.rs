9b55e122972499d508764b0b27e7ec2c2f201ba8
smalnote <smalnote@gmail.com>
2024-09-13 12:10:03 +0800

    hello_marco: misc
    Reexport `hello_marco_derive` in `hello_marco`
    Make `hello_marco_derive` an optional feature
    Import `hello_marco` with `derive` feature

--- hello_marco/src/lib.rs
+++ hello_marco/src/lib.rs
@@ -1,3 +1,8 @@
 pub trait HelloMarco {
     fn hello_marco();
 }
+
+// has the same use path as `trait Hello`
+// When used in #[derive(HelloMarco)], refers to this type
+#[cfg(feature = "derive")]
+pub use ::hello_marco_derive::HelloMarco;
