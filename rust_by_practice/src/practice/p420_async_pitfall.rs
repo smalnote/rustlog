47b7cafdcb9de18cc11f90a4048f7ff813743cdf
smalnote <smalnote@gmail.com>
2024-11-02 16:38:34 +0800

    async: memory pin

--- rust_by_practice/src/practice/p420_async_pitfall.rs
+++ rust_by_practice/src/practice/p420_async_pitfall.rs
@@ -59,4 +59,56 @@ mod tests {
         let sleep_futures = (1..=10).map(|id| sleep_ms(&start, id, id * 10));
         join_all(sleep_futures).await;
     }
+
+    /// Async block and function's returned local variables are wrapped inside future,
+    /// some of those variables can hold pointers to other local variables. Because of
+    /// that, the future should never be moved to a different memory location, as it
+    /// would invalidate those pointers.
+    /// `Pin` prevents moving the wrapped variables in memory.
+    /// Data that contains pointers to itself is called self-referential.
+    /// `Pin` is a wrapper around a reference. And object cannot be moved from its place
+    /// using a pinned pointer. However, it can still be moved through an unpinned pointer.
+    /// The `poll` method of the `Future` trait uses `Pin<&mut Self>` instead of `&mut Self`
+    /// to refer to the instance. That's why it can only be called on a pinned pointer.
+    #[tokio::test]
+    async fn pin_variables_in_memory() {
+        use tokio::sync::{mpsc, oneshot};
+        use tokio::task::spawn;
+        use tokio::time::{sleep, Duration};
+        let (sender, receiver) = mpsc::channel(10);
+        spawn(worker(receiver));
+        for i in 0..100 {
+            let response = do_work(&sender, i).await;
+            println!("work result for iteration {}: {}", i, response);
+        }
+
+        struct Work {
+            input: u32,
+            respond_on: oneshot::Sender<u32>,
+        }
+
+        async fn worker(mut receiver: mpsc::Receiver<Work>) {
+            // the `Future` returned by sleep() can only be called on a pinned pointer
+            let mut timeout_future = Box::pin(sleep(Duration::from_millis(100)));
+            loop {
+                tokio::select! {
+                    Some(work) = receiver.recv() => {
+                        sleep(Duration::from_millis(10)).await;
+                        work.respond_on.send(work.input * 1000).unwrap();
+                    },
+                    _ = &mut timeout_future => {
+                        println!("worker timeout");
+                        timeout_future = Box::pin(sleep(Duration::from_millis(100)));
+                    },
+                }
+            }
+        }
+
+        async fn do_work(sender: &mpsc::Sender<Work>, input: u32) -> u32 {
+            let (respond_on, receiver) = oneshot::channel();
+            let work = Work { input, respond_on };
+            sender.send(work).await.unwrap();
+            receiver.await.unwrap()
+        }
+    }
 }
