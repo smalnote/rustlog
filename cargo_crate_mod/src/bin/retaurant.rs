2712516f0918681ac664ea85d7af92c44c543014
smalnote <smalnote@gmail.com>
2024-12-03 12:27:04 +0800

    bin_mod: mod within  bin

--- cargo_crate_mod/src/bin/retaurant.rs
+++ cargo_crate_mod/src/bin/retaurant.rs
@@ -2,9 +2,11 @@
 /// `crate::`, make the src/bin as a external user of mods of src/*.
 use cargo_crate_mod::restaurant;
 
+// mod from ./bin_mod/mod.rs is used as a internal mod.
+mod bin_mod;
+
 // Run this binary crate with command: cargo run --bin restaurant
 fn main() {
-    // hierachy version of mod restaurant in directory src/restaurant/*
     restaurant::front_of_house::hosting::add_to_waitlist();
     restaurant::front_of_house::hosting::seat_at_table();
     restaurant::front_of_house::serving::take_order();
@@ -14,4 +16,5 @@ fn main() {
     restaurant::back_of_house::cooking::cook_fish();
     restaurant::back_of_house::cleaning::wash_dish();
     restaurant::back_of_house::cleaning::sweep_floor();
+    bin_mod::restaurant::post_cleaning();
 }
