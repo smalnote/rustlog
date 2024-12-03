17bf069868d0bd3e48034e9733512962fe2a05ad
smalnote <smalnote@gmail.com>
2024-12-03 12:05:53 +0800

    lsc: separate mod example

---
+++ cargo_crate_mod/src/arithmetic/mod.rs
@@ -0,0 +1,13 @@
+/// Add one to the input value and return the new value
+///
+/// # Examples
+///
+/// ```
+/// let x = 41;
+/// let y = cargo_crate_mod::arithmetic::add_one(x);
+///
+/// assert_eq!(y, 42);
+/// ```
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
---
+++ cargo_crate_mod/src/bin/retaurant.rs
@@ -0,0 +1,17 @@
+/// Binary in src/bin have to import mod from src/* */ with a package name instead
+/// `crate::`, make the src/bin as a external user of mods of src/*.
+use cargo_crate_mod::restaurant;
+
+// Run this binary crate with command: cargo run --bin restaurant
+fn main() {
+    // hierachy version of mod restaurant in directory src/restaurant/*
+    restaurant::front_of_house::hosting::add_to_waitlist();
+    restaurant::front_of_house::hosting::seat_at_table();
+    restaurant::front_of_house::serving::take_order();
+    restaurant::front_of_house::serving::serve_order();
+    restaurant::front_of_house::serving::take_payment();
+    restaurant::back_of_house::cooking::fire_tomato();
+    restaurant::back_of_house::cooking::cook_fish();
+    restaurant::back_of_house::cleaning::wash_dish();
+    restaurant::back_of_house::cleaning::sweep_floor();
+}
