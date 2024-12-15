0de6170486a9d304b954e00a95210abfa4dec359
smalnote <smalnote@gmail.com>
2024-12-16 00:59:32 +0800

    zero_copy: libc sendfile64

--- rust_by_practice/src/bin/zero_copy/main.rs
+++ rust_by_practice/src/bin/zero_copy/main.rs
@@ -2,7 +2,10 @@ use std::{
     env,
     fs::File,
     io::{self, Read, Result, Write},
-    os::unix::net::UnixStream,
+    os::{
+        fd::AsRawFd,
+        unix::{fs::MetadataExt, net::UnixStream},
+    },
     time::Instant,
 };
 
@@ -24,6 +27,7 @@ fn main() -> Result<()> {
         "std_io_copy" => copy_file_to_unix_domain_socket(file, socket)?,
         "nix_sendfile" => nix_sendfile(file, socket)?,
         "read_write" => read_write(file, socket)?,
+        "libc_sendfile" => libc_sendfile(file, socket)?,
         _ => panic!("unsupported method {}", method),
     }
 
@@ -53,6 +57,26 @@ fn read_write(file: File, socket: UnixStream) -> Result<()> {
             break;
         }
         (&socket).write_all(&buf[..bytes_read])?
+    }
+    Ok(())
+}
+
+fn libc_sendfile(file: File, socket: UnixStream) -> Result<()> {
+    let mut len_written = 0_u64;
+    let len_file = file.metadata().unwrap().size();
+    while len_written < len_file {
+        let chunck_size = std::cmp::min(len_file - len_written, 0x7ffff000_u64) as usize;
+        match unsafe {
+            libc::sendfile64(
+                socket.as_raw_fd(),
+                file.as_raw_fd(),
+                std::ptr::null_mut(),
+                chunck_size,
+            )
+        } {
+            -1 => return Err(std::io::Error::last_os_error()),
+            len_sent => len_written += len_sent as u64,
+        }
     }
     Ok(())
 }
