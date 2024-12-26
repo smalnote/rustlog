3e7ee5cee6dd76bb36c20381c35beff83a20a82e
smalnote <smalnote@gmail.com>
2024-12-26 13:25:09 +0800

    leeetcode: 494 target sum

--- leetcode/src/lib.rs
+++ leetcode/src/lib.rs
@@ -1,5 +1,7 @@
 pub mod p1456_max_vowels;
 pub mod p3203_find_minimum_diameter_after_mergin_two_trees;
 pub mod p46_permutations;
+#[path = "./494.target-sum.rs"]
+pub mod p494_target_sum;
 pub mod p515_find_largest_value_in_each_tree_row;
 pub mod p89_gray_code;
