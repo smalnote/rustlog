3003ccccb690b9f255bf1dc939b90b49efd432b4
smalnote <smalnote@gmail.com>
2025-04-21 10:48:06 +0800

    grpc_hello: add relection service

--- grpc_hello/src/server.rs
+++ grpc_hello/src/server.rs
@@ -5,9 +5,15 @@ use hello_world::greeter_server::{Greeter, GreeterServer};
 use hello_world::{HelloReply, HelloRequest};
 use tonic::transport::Server;
 use tonic::{Request, Response, Status};
+use tonic_reflection::server::Builder;
 
 pub mod hello_world {
     tonic::include_proto!("helloworld");
+}
+
+pub mod proto {
+    pub(crate) const FILE_DESCRIPTOR_SET: &[u8] =
+        tonic::include_file_descriptor_set!("helloworld_descriptor");
 }
 
 #[derive(Default)]
@@ -41,8 +47,14 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     println!("GreeterServer listening on {}", addr);
 
+    let reflection_service = Builder::configure()
+        .register_encoded_file_descriptor_set(proto::FILE_DESCRIPTOR_SET)
+        .build_v1()
+        .unwrap();
+
     Server::builder()
         .add_service(GreeterServer::new(greeter))
+        .add_service(reflection_service)
         .serve(addr)
         .await?;
 
