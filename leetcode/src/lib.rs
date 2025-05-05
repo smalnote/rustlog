ad9b826ea88f295d5fb9408606dc383aaa470dc6
smalnote <smalnote@gmail.com>
2025-05-05 17:47:39 +0800

    leetcode: 50 pow

--- leetcode/src/lib.rs
+++ leetcode/src/lib.rs
@@ -7,6 +7,8 @@ pub mod p3203_find_minimum_diameter_after_mergin_two_trees;
 pub mod p46_permutations;
 #[path = "./494.target-sum.rs"]
 pub mod p494_target_sum;
+#[path = "./50.pow.rs"]
+pub mod p50;
 pub mod p515_find_largest_value_in_each_tree_row;
 #[path = "./740.delete-and-earn.rs"]
 pub mod p740_delete_and_earn;
